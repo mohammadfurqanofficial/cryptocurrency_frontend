eb3204076a483a0e39dc5ff22ac9bd8adfdcbced
Mohammad Furqan <mohammadfurqanofficial@gmail.com>
2024-10-09 18:05:35 +0500

    Added coin hisotry data added in console

--- src/pages/coin/[id].tsx
+++ src/pages/coin/[id].tsx
@@ -96,7 +96,7 @@ const CoinDetails = () => {
     setCsvAllLoading(true); // Use separate loading state for "All history" button
     try {
       const response = await api.get(`/coins/coin-history/${id}`);
-      console.log(response.data.coinHistory);
+      console.log("Coin history data", response.data.coinHistory);
       // Validate the response format
       if (response.status === 200 && response.data && Array.isArray(response.data.coinHistory)) {
         const allHistoryData = response.data.coinHistory.map((history: CoinHistory) => ({
